facf99c7de37cb6c7e946bcbdb18983057071c75
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-04 00:41:40 -0300

    Improved Print in Reverse - Linked List

--- hackerrank/reverse_print_ll.py
+++ hackerrank/reverse_print_ll.py
@@ -1,12 +1,5 @@
 def ReversePrint(head):
     if not head:
         return
-    
-    nodes = []
-    while head.next:
-        nodes.append(head.data)
-        head = head.next
-    nodes.append(head.data)
-    
-    for i in reversed(nodes):
-        print i
+    ReversePrint(head.next)
+    print head.data
