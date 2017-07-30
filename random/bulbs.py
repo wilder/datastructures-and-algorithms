e6d022a7707067056231c98eccc7fe34d3d04a56
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-30 20:43:14 -0300

    improved to O(n)

--- random/bulbs.py
+++ random/bulbs.py
@@ -1,38 +1,20 @@
-def change_state(bulbs):
-    for i, _ in enumerate(bulbs):
-        if bulbs[i] == 1:
-            bulbs[i] = 0
-        else:
-            bulbs[i] = 1
-
-def bulbs(A):
-    counter = 0
-    for i, _ in enumerate(A):
-         if A[i] == 0class Solution:
+class Solution:
     # @param A : list of integers
     # @return an integer
-    def change_state(self, bulbs):
-        for b, _ in enumerate(bulbs):
-            if bulbs[b] == 1:
-                bulbs[b] = 0
-            else:
-                bulbs[b] = 1
-        return bulbs
-    
+    # [0, 1, 0, 1]
     def bulbs(self, A):
-        if not A or not 0 in A:
-            return 0
         counter = 0
         for i, _ in enumerate(A):
+            if counter & 1:
+                if A[i] == 0:
+                    A[i] = 1
+                else:
+                    A[i] = 0
+            
             if A[i] == 0:
-                A[i:] = self.change_state(A[i:])
                 counter+=1
-        return counter:
-             print 'before changing', A
-             change_state(A[i:])
-             counter+=1
-             print 'changed', A
-    return counter
-
-a = [0, 1, 0, 1]
-print bulbs(a)
+                A[i] = 1
+            else:
+                A[i] = 0
+                
+        return counter
