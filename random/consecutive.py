36c1a4d186c933a3bf95fc87674edc453fe43a3b
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-31 22:45:15 -0300

    Improved to O(n)

--- random/consecutive.py
+++ random/consecutive.py
@@ -6,26 +6,35 @@ to the target.
 Example: a = {1, 3, 5, 7, 9}
 target 8
 output = true ({3,5})
+
 '''
 
 def consecutive(a, target):
     if not a:
         return False
     
+    #will be the index of the first non-removed element
+    low = 0
     i = 0
-    while i < len(a)-1:
-        tracksum = a[i]
-        nums = [a[i]] 
-        print 'eta'
-        for j in a[i+1:]:
-            if (tracksum + j) <= target:
-                tracksum+=j
-                nums.append(j)
-            if tracksum == target:
-                print 'found ', nums
-                return True
-        i+=1
+    s = 0
 
+    while i < len(a):
+
+        if s > target:
+            #remove the first non-removed element
+            s-=a[low]
+            low+=1
+            continue
+
+        if s == target:
+            print a[low:i]
+            return True 
+        
+        s+=a[i]
+        i+=1
+    
+    return False
+        
 a = [1, 3, 5, 7, 9]
 target = 12
 
