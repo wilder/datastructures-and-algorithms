37106ba485905c5c93aa8e56e8bbdb405bcd0fc9
Wilder Pereira <wilder_roberto@hotmail.com>
2017-08-08 22:18:32 -0300

    improved solution

--- random/fb_balanced_parentheses.py
+++ random/fb_balanced_parentheses.py
@@ -1,13 +1,32 @@
-def solution(s):
-	balanced = ''
+def is_balanced(s):
 	stk = []
 	for i in s:
-		if i != ')':
-			balanced += i
-			if i == '(':
-				stk.append(i)
-		else:
+		if i == '(':
+			stk.append(i)
+		if i == ')':
 			if stk:
 				stk.pop()
-				balanced += i
+			else:
+				return False
+	return True
+
+def solution(s):
+	balanced = ''
+	if is_balanced(s):
+		return s
+	else:
+		stk = []
+		lstk = []
+		for i in s:
+			if i != ')':
+				if i == '(':
+					stk.append(i)
+				else:
+					lstk.append(i)
+			else:
+				if stk:
+					balanced+=stk.pop()
+					if lstk:
+						balanced+=lstk.pop()
+					balanced+= i
 	return balanced
