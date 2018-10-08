26a459f8076a10860b5357bea6451877cdff0720
Wilder Pereira <wilder_roberto@hotmail.com>
2018-10-08 00:02:17 -0300

    Add Hackerrank Fibonaci Modified - Dynamic Programming - 100%

---
+++ hackerrank/fibonacci_modified.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    memo = [t1, t2]
+
+    for i in range(2, n):
+        memo[i] = memo[i-2] + pow(2, memo[i-1])
+
+    return memo[n-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
