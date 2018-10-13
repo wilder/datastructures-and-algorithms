d8b5c57fe02ff4f3602ddd97c74ad1f002c58fe2
Wilder Pereira <wilder_roberto@hotmail.com>
2018-10-13 17:56:53 -0300

    Improve compress-decompression

--- google-advanced-programming/compress-decompression.py
+++ google-advanced-programming/compress-decompression.py
@@ -1,30 +1,39 @@
 def decompress(compressed_str):
 
-    decompressed = ''
     num_str = ''
     num_queue = []
+    decompressed = ''
     curr_str = ''
+
+    brackets = []
+    results = []
 
     index = 0
     while index < len(compressed_str):
         while compressed_str[index].isdigit():
             num_str += compressed_str[index]
             index += 1
-        
+
         if compressed_str[index] == '[':
             num_queue.append(int(num_str))
             num_str = ''
+            brackets.append(compressed_str[index])
 
         elif compressed_str[index] == ']':
+            brackets.pop()
             decompressed += curr_str * num_queue.pop()
             curr_str = ''
+
+            if not brackets:
+                results.append(decompressed)
+                decompressed = ''
 
         else:
             curr_str += compressed_str[index]
 
         index += 1
 
-    return decompressed + curr_str
+    return ''.join(results) + curr_str
 
 if __name__ == "__main__":
     print decompress('3[abc]4[ab]c')
