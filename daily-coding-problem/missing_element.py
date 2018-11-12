a9c22595fcd24f9e1924c767cdd8ae287f1bd959
Wilder Pereira <wilder_roberto@hotmail.com>
2018-11-11 22:10:20 -0200

    Improve DCP #4 - Missing Element to O(n)

--- daily-coding-problem/missing_element.py
+++ daily-coding-problem/missing_element.py
@@ -4,24 +4,28 @@ Given an array of integers, find the first missing positive integer in linear ti
 For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 '''
 
-#Time: O(n log n) and Space: O(1) solution
+'''
+O(n) time solution - O(n) space.
+Create a hash table with the the key as the number and value a boolean.
+For each of the numbers in the array, add it to the dict
+For each of the elements in the array, check if element+1 is in the dict
+if not, check if it is smallest than the last missing
+return the smallest missing element
+'''
+import sys
 def missing_element(array):
+    index = dict([element, True] for element in array)
+    lowest_missing = sys.maxint
+    for element in array:
+        if element < 0:
+            continue
+        if element + 1 not in index:
+            if element + 1 < lowest_missing:
+                lowest_missing = element + 1
+    return lowest_missing
+        
     
-    if not array:
-        return None
-    
-    array.sort()
-    last = array[0]
-    for index in range(1, len(array)):
-        current = array[index]
-        if current > 0 and current - last > 1:
-            return last + 1
-        if current < 0:
-            last = 0
-        else:
-            last = current
-    
-    return array[-1] + 1
+
 
 assert missing_element([3,1,3,5,1,3,3,3,3,1,2,3,5,5,0,1,2,3,1,2]) == 4
 assert missing_element([-1, -4, 3, 3, 2, 0, 2, 5]) == 1
