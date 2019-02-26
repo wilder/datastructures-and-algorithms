df25f1e3f5483188a1d5ac4df59bc23a8177c808
Wilder Pereira <wilder_roberto@hotmail.com>
2019-02-26 00:07:36 -0300

    Improve LowestCommonAncestor - Faster than 100% of Java leetcode submissions

--- daily-coding-problem/112-Twitter-LowestCommonAncestor/LowestCommonAncestor.java
+++ daily-coding-problem/112-Twitter-LowestCommonAncestor/LowestCommonAncestor.java
@@ -6,44 +6,34 @@
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
  *
  */
-
 class LowestCommonAncestor {
-
-    public TreeNode lca = null;
-
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        traverse(root, p, q);
-        return lca;
+        
+        if(root == null)
+            return null;
+        
+        TreeNode ancestor = null;
+        if (root.val == p.val || root.val == q.val)
+            ancestor = root;
+        
+        TreeNode leftAncestor = lowestCommonAncestor(root.left, p, q);        
+        TreeNode rightAncestor = lowestCommonAncestor(root.right, p, q);
+
+        
+        if (ancestor != null && ((rightAncestor != null) || (leftAncestor != null))) {
+            return root;
+        }
+        
+        if ((rightAncestor != null) && (leftAncestor != null)) {
+            return root;
+        }
+        
+        if(ancestor != null)
+            return root;
+        
+        return leftAncestor != null ? leftAncestor : rightAncestor;
+        
     }
-
-    public boolean traverse(TreeNode root, TreeNode p, TreeNode q) {
-
-        boolean isTarget = false;
-        if (root.val == p.val || root.val == q.val) {
-            isTarget = true;
-        }
-
-        boolean leftVisitedTarget = false;
-        System.out.println("Visiting "+root.val);
-        if(root.left != null) {
-            leftVisitedTarget = traverse(root.left, p, q);
-            System.out.println("Back to "+root.val + " from " + root.left.val + " leftVisitedTarget: " + leftVisitedTarget);
-        }
-
-        boolean rightVisitedTarget = false;
-        if(root.right != null) {
-            rightVisitedTarget = traverse(root.right, p, q);
-            System.out.println("Back to "+root.val + " from " + root.right.val + " rightVisitedTarget: " + rightVisitedTarget);
-        }
-
-        if ((isTarget && (leftVisitedTarget||rightVisitedTarget))
-             || (leftVisitedTarget && rightVisitedTarget) && lca == null) {
-            System.out.println("I'm the LCA! "+ root.val);
-            lca = root;
-        }
-
-        return isTarget || leftVisitedTarget || rightVisitedTarget;
-
-    }
-
+    
 }
