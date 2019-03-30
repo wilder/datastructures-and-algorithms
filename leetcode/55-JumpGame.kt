7887121d23f770f373a93456d3f59cb72e20b49e
Wilder Pereira <wilder_roberto@hotmail.com>
2019-03-30 13:41:57 -0300

    Solve LeetCode's JumpGame faster than 100% of Kotlin's  subimissions

--- leetcode/55-JumpGame.kt
+++ leetcode/55-JumpGame.kt
@@ -3,38 +3,25 @@
  * https://leetcode.com/problems/jump-game/submissions/
  */
 class Solution {
-
+    
     fun canJump(nums: IntArray): Boolean {
-
-        val memo = MutableList<Boolean>(nums.size) {false}
-        memo[nums.size - 1] = true
-
+        
+        var targetPosition = nums.size - 1 
+        
         var index = nums.size - 2
-
+        
         while (index >= 0) {
-            memo[index] = canReach(index, nums, memo)
+            val jumpLength = nums[index]
+            
+            if (jumpLength + index >= targetPosition) {
+                targetPosition = index
+            }
+            
             index -= 1
         }
-
-        return memo[0]
-
+        
+        return targetPosition == 0
+                       
     }
-
-    /*
-     * Can be improved by updating the target position every time it can reach the target. Then, only check if the current jump is greater than the target.
-     */
-    fun canReach(index: Int, nums: IntArray, memo: List<Boolean>): Boolean {
-        for (jump in 0..nums[index]) {
-            if (jump >= nums.size-1) {
-                return true
-            } else {
-                val canReachFromCurrent = memo[index + jump]
-                if (canReachFromCurrent) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-
+    
 }
